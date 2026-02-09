baeda1e51d7e98529c08b86be5da910dbf951411
Adammouedden <adam.mouedden@gmail.com>
2026-02-09 17:31:08 +0000

    API-david | Changed db.php's mysql login back to root, tested most endpoints successfully

--- backend/db.php
+++ backend/db.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL);
 
 
 $host = "localhost";
-$user = "contact_user";
-$password = "contact_pass";
+$user = "root";
+$password = "team3";
 $db = "contactManager";
 
 $conn = new mysqli($host, $user, $password, $db);
