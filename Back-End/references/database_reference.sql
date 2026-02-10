c4342535ceb36ee7bf7561cbd707b1354ec1577d
ale0614 <alrenellij@gmail.com>
2026-02-10 14:42:50 -0500

    Change in database

--- Back-End/references/database_reference.sql
+++ Back-End/references/database_reference.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     phone VARCHAR(20),
     password_user VARCHAR(255) NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -16,7 +16,7 @@ CREATE TABLE contacts(
     id INT AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
     phone VARCHAR(20),
     user_id INT NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
