384bad365790bdb1ba6c373a724106fbf68e6330
Adammouedden <146101324+Adammouedden@users.noreply.github.com>
2026-02-13 02:56:38 -0500

    API | patches syntax error on db.php

--- API/scripts/db.php
+++ API/scripts/db.php
@@ -18,6 +18,6 @@ if ($conn->connect_error)
 
 else
 {
-    echo json_encode({"status"=>"success", "message"=>"Successful connection to mySQL database"});
+    echo json_encode(["status"=>"success", "message"=>"Successful connection to mySQL database"]);
 }
 ?>
